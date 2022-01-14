5009c099fea5432fc36eb69d6e0596d562ed4d20
Justin Seda <jseda3237@gmail.com>
2022-01-14 00:44:49 -0500

    Working on a Twitter clone

---
+++ src/Sidebar.js
@@ -0,0 +1,17 @@
+// import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Sidebar() {
+	return (
+		<div className="sidebar">
+			{/* Twiter Icon */}
+			<TwitterIcon />
+
+			{/* Sidebar Option*/}
+
+			{/* Button -> Tweet */}
+		</div>
+	);
+}
+
+export default Sidebar;
